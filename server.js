29073d4fe3275888a11954db98e62ae94c28e240
kathulhur <josephkarl19@outlook.com>
2021-08-19 17:55:40 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const path      = require('path');
+const http      = require('http');
+const express   = require('express');
+const socketio  = require('socket.io');
+
+const app       = express();
+const server    = http.createServer(app);
+const io        = socketio(server);
+
+const PORT = 3000 || process.env.PORT;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log("New WS Connection...");
+
+    socket
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
